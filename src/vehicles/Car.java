8f6f451888a342fb5d61da82e0246b0c0e34f5ff
Kidus alemauehu <kidusa430@gmail.com>
2026-05-29 02:45:01 -0700

    Added GitHub Actions pipeline

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -3,10 +3,10 @@ package vehicles;
 public class Car extends Vehicle {
 
     private int doors;
-
     public Car(String brand, int speed, int doors) {
-        super(brand, speed);
+        super(brand, speed );
         this.doors = doors;
+
     }
 
     // Method Overriding
@@ -17,5 +17,6 @@ public class Car extends Vehicle {
         System.out.println("Brand: " + getBrand());
         System.out.println("Speed: " + getSpeed());
         System.out.println("Doors: " + doors);
+
     }
 }
